9cc3a75eaeadcde8a53f7140e46de1314c33b973
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-28 01:14:01 -0300

    trabajando en el componente Hero

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <section>
-    <h2>last News</h2>
-    <button>Browser articles</button>
-    <Image src="/user.png" alt="user" width={300} height={300} />
-  </section>;
+  return (
+    <section>
+      <h2>last News</h2>
+      <button>Browser articles</button>
+      <Image src="/user.png" alt="user" width={300} height={300} />
+    </section>
+  );
 };
 
 export default Hero;
