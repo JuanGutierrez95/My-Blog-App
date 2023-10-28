ffdd4b55448e1323e4993c26c6c9eb4a35b83845
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-28 00:35:58 -0300

    trabajando en el componente Hero

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -5,6 +5,8 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return <section>
+    <h2>last News</h2>
+    <button>Browser articles</button>
     <Image src="/user.png" alt="user" width={300} height={300} />
   </section>;
 };
