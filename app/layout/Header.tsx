e78919b287938e0ded7e1a4dde73dedb9f002764
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-27 23:58:22 -0300

    componente Header

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -3,10 +3,12 @@ import React from "react";
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div className="px-2 py-4 border-b">
-    <div className="text-4xl px-2 py-4">JG</div>
-    <button className="text-white bg-black px-2 py-4">Sign In</button>
-    </div>;
+  return (
+    <div className="px-2 py-4 border-b border-white flex justify-between">
+      <div className="text-4xl px-2 py-4">JG</div>
+      <button className="text-white bg-black px-4 py-2">Sign In</button>
+    </div>
+  );
 };
 
 export default Header;
