b91216b4d9e2c0e8b7d231365f298e8940111d5a
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-10-27 23:42:53 -0300

    trabajando en el componente Header

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 const Header = (props: Props) => {
   return <div className="px-2 py-4 border-b">
-    <div className="text-4xl">JG</div>
-    <div></div>
+    <div className="text-4xl px-2 py-4">JG</div>
+    <button className="text-white bg-black px-2 py-4">Sign In</button>
     </div>;
 };
 
