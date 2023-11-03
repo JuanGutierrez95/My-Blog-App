d31581e78b15d29eee41c5155874070cf8f24eb1
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-02 23:53:23 -0300

    avanzando en el proyecto

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -6,11 +6,15 @@ type Props = {};
 const Hero = (props: Props) => {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-12">
-      <div className="col-span-7">
-      <h1 className="text-2xl sm:text-4xl">last News</h1>
-      <button>Browser articles</button>
+      <div className="col-span-7 place-self-center text-center sm:text-left justifity-self-start">
+        <h1 className="text-2xl sm:text-4xl md:text-6xl md:leading-normal">
+          Last News
+        </h1>
+        <button className="text-white bg-indigo-500 px-4 py-2 border-2 rounded shadow-[0.25rem]">Browser articles</button>
       </div>
-      <Image src="/user.png" alt="user" width={300} height={300} />
+      <div className="col-span-5 place-self-center">
+        <Image src="/user.png" alt="user" width={300} height={300} />
+      </div>
     </section>
   );
 };
