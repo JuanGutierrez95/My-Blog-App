00c963005f15d4f96c47eff6613c1d7eaabaab20
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-03 00:39:16 -0300

    configurando tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
     container: {
       center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1300px",
+      },
     },
     borderColor: {
       DEFAULT: "black",
