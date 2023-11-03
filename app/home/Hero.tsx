11de5a592a95f88d9bdfbec3f564c30aaa92679a
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-03 00:25:38 -0300

    trabajando en el componente hero

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -5,12 +5,12 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-12">
+    <section className="grid grid-cols-1 sm:grid-cols-12 mt-6">
       <div className="col-span-7 w-full place-self-center text-center sm:text-left justifity-self-start">
         <h1 className="text-2xl sm:text-4xl md:text-6xl md:leading-normal">
           Last News
         </h1>
-        <button className="text-white bg-indigo-500 px-6 py-4 border-2 rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]">
+        <button className="text-white bg-indigo-500 px-4 py-2 px-sm:px-6 sm:py-4 mt-3 border-2 rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]">
           Browser articles
         </button>
       </div>
